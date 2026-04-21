0b515277426180e227e4484de71a4fb3ef34b1ae
crow <pengchengw@me.com>
2026-04-21 17:17:01 -0500

    Revert "Handle Archimonde air burst target interrupt during spread (#15)"
    This reverts commit 148eb911757ebfad8111c61728781f4d450cc891.

--- src/Ai/Raid/HyjalSummit/Action/RaidHyjalSummitActions.cpp
+++ src/Ai/Raid/HyjalSummit/Action/RaidHyjalSummitActions.cpp
@@ -985,15 +985,12 @@ bool ArchimondeSpreadToAvoidAirBurstAction::Execute(Event /*event*/)
             static_cast<uint32>(HyjalSummitSpells::SPELL_AIR_BURST))
         {
             Player* mainTank = GetGroupMainTank(botAI, bot);
-            if (mainTank && spell->m_targets.GetUnitTarget() == bot)
+            if (mainTank && spell->m_targets.GetUnitTarget() == mainTank)
             {
                 float currentDistance = bot->GetDistance2d(mainTank);
                 constexpr float safeDistance = 14.0f;
                 if (currentDistance < safeDistance)
-                {
-                    botAI->RequestSpellInterrupt();
                     return MoveAway(mainTank, safeDistance - currentDistance);
-                }
             }
         }
     }
