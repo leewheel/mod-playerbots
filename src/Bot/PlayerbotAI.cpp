31f8e42a5421a8bc67f9ce5a75cac2c2ad79ef42
crow <pengchengw@me.com>
2026-05-12 20:35:56 -0500

    fix saytoraid

--- src/Bot/PlayerbotAI.cpp
+++ src/Bot/PlayerbotAI.cpp
@@ -2964,7 +2964,7 @@ bool PlayerbotAI::SayToParty(const std::string& msg)
 
 bool PlayerbotAI::SayToRaid(const std::string& msg)
 {
-    if (!bot->GetGroup() || bot->GetGroup()->isRaidGroup())
+    if (!bot->GetGroup() || !bot->GetGroup()->isRaidGroup())
         return false;
 
     WorldPacket data;
