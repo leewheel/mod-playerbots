8469bf3a66ddaf4ada83cd6fd0fd652ee0e33b5c
crow <pengchengw@me.com>
2026-06-13 18:16:26 -0500

    prune expired notice times for out of reagents announce

--- src/Ai/Base/Util/GenericBuffUtils.h
+++ src/Ai/Base/Util/GenericBuffUtils.h
@@ -32,10 +32,10 @@ bool ShouldDeferGreaterBlessingAssignmentForRecentLogin(Player* bot);
 
 bool HasRequiredReagents(Player* bot, uint32 spellId);
 
+void ClearMissingBuffReagentNotice(Player* bot, std::string const& groupName);
+
 bool TryAnnounceMissingBuffReagents(
-    PlayerbotAI* botAI,
-    std::string const& baseName,
-    std::string const& groupName);
+    PlayerbotAI* botAI, std::string const& baseName, std::string const& groupName);
 
 std::string UpgradeToGroupIfAppropriate(
     Player* bot,
