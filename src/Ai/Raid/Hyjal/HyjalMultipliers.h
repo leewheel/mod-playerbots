7044d9b982962a45ac18cf4d0c4c6066ce765a57
crow <pengchengw@me.com>
2026-08-10 21:02:13 -0500

    change formatting of hyjal .h files

--- src/Ai/Raid/Hyjal/HyjalMultipliers.h
+++ src/Ai/Raid/Hyjal/HyjalMultipliers.h
@@ -12,8 +12,8 @@
 class HyjalSummitTimeBloodlustAndHeroismMultiplier : public Multiplier
 {
 public:
-    HyjalSummitTimeBloodlustAndHeroismMultiplier(
-        PlayerbotAI* botAI) : Multiplier(botAI, "hyjal summit time bloodlust and heroism multiplier") {}
+    HyjalSummitTimeBloodlustAndHeroismMultiplier(PlayerbotAI* botAI)
+        : Multiplier(botAI, "hyjal summit time bloodlust and heroism multiplier") {}
     float GetValue(Action* action) override;
 };
 
@@ -22,16 +22,16 @@ public:
 class RageWinterchillDisableCombatFormationMoveMultiplier : public Multiplier
 {
 public:
-    RageWinterchillDisableCombatFormationMoveMultiplier(
-        PlayerbotAI* botAI) : Multiplier(botAI, "rage winterchill disable combat formation move multiplier") {}
+    RageWinterchillDisableCombatFormationMoveMultiplier(PlayerbotAI* botAI)
+        : Multiplier(botAI, "rage winterchill disable combat formation move multiplier") {}
     float GetValue(Action* action) override;
 };
 
 class RageWinterchillMeleeControlAvoidanceMultiplier : public Multiplier
 {
 public:
-    RageWinterchillMeleeControlAvoidanceMultiplier(
-        PlayerbotAI* botAI) : Multiplier(botAI, "rage winterchill melee control avoidance multiplier") {}
+    RageWinterchillMeleeControlAvoidanceMultiplier(PlayerbotAI* botAI)
+        : Multiplier(botAI, "rage winterchill melee control avoidance multiplier") {}
     float GetValue(Action* action) override;
 };
 
@@ -40,24 +40,24 @@ public:
 class AnetheronDisableTankActionsMultiplier : public Multiplier
 {
 public:
-    AnetheronDisableTankActionsMultiplier(
-        PlayerbotAI* botAI) : Multiplier(botAI, "anetheron disable tank actions multiplier") {}
+    AnetheronDisableTankActionsMultiplier(PlayerbotAI* botAI)
+        : Multiplier(botAI, "anetheron disable tank actions multiplier") {}
     float GetValue(Action* action) override;
 };
 
 class AnetheronDisableCombatFormationMoveMultiplier : public Multiplier
 {
 public:
-    AnetheronDisableCombatFormationMoveMultiplier(
-        PlayerbotAI* botAI) : Multiplier(botAI, "anetheron disable combat formation move multiplier") {}
+    AnetheronDisableCombatFormationMoveMultiplier(PlayerbotAI* botAI)
+        : Multiplier(botAI, "anetheron disable combat formation move multiplier") {}
     float GetValue(Action* action) override;
 };
 
 class AnetheronControlMisdirectionMultiplier : public Multiplier
 {
 public:
-    AnetheronControlMisdirectionMultiplier(
-        PlayerbotAI* botAI) : Multiplier(botAI, "anetheron control misdirection multiplier") {}
+    AnetheronControlMisdirectionMultiplier(PlayerbotAI* botAI)
+        : Multiplier(botAI, "anetheron control misdirection multiplier") {}
     float GetValue(Action* action) override;
 };
 
@@ -66,24 +66,24 @@ public:
 class KazrogalLowManaBotStayAwayFromGroupMultiplier : public Multiplier
 {
 public:
-    KazrogalLowManaBotStayAwayFromGroupMultiplier(
-        PlayerbotAI* botAI) : Multiplier(botAI, "kaz'rogal low mana bot stay away from group multiplier") {}
+    KazrogalLowManaBotStayAwayFromGroupMultiplier(PlayerbotAI* botAI)
+        : Multiplier(botAI, "kaz'rogal low mana bot stay away from group multiplier") {}
     float GetValue(Action* action) override;
 };
 
 class KazrogalKeepAspectOfTheViperActiveMultiplier : public Multiplier
 {
 public:
-    KazrogalKeepAspectOfTheViperActiveMultiplier(
-        PlayerbotAI* botAI) : Multiplier(botAI, "kaz'rogal keep aspect of the viper active multiplier") {}
+    KazrogalKeepAspectOfTheViperActiveMultiplier(PlayerbotAI* botAI)
+        : Multiplier(botAI, "kaz'rogal keep aspect of the viper active multiplier") {}
     float GetValue(Action* action) override;
 };
 
 class KazrogalControlMovementMultiplier : public Multiplier
 {
 public:
-    KazrogalControlMovementMultiplier(
-        PlayerbotAI* botAI) : Multiplier(botAI, "kaz'rogal control movement multiplier") {}
+    KazrogalControlMovementMultiplier(PlayerbotAI* botAI)
+        : Multiplier(botAI, "kaz'rogal control movement multiplier") {}
     float GetValue(Action* action) override;
 };
 
@@ -92,24 +92,24 @@ public:
 class AzgalorDisableTankActionsMultiplier : public Multiplier
 {
 public:
-    AzgalorDisableTankActionsMultiplier(
-        PlayerbotAI* botAI) : Multiplier(botAI, "azgalor disable tank actions multiplier") {}
+    AzgalorDisableTankActionsMultiplier(PlayerbotAI* botAI)
+        : Multiplier(botAI, "azgalor disable tank actions multiplier") {}
     float GetValue(Action* action) override;
 };
 
 class AzgalorDoomedBotPrioritizePositioningMultiplier : public Multiplier
 {
 public:
-    AzgalorDoomedBotPrioritizePositioningMultiplier(
-        PlayerbotAI* botAI) : Multiplier(botAI, "azgalor doomed bot prioritize positioning multiplier") {}
+    AzgalorDoomedBotPrioritizePositioningMultiplier(PlayerbotAI* botAI)
+        : Multiplier(botAI, "azgalor doomed bot prioritize positioning multiplier") {}
     float GetValue(Action* action) override;
 };
 
 class AzgalorMeleeDpsControlAvoidanceMultiplier : public Multiplier
 {
 public:
-    AzgalorMeleeDpsControlAvoidanceMultiplier(
-        PlayerbotAI* botAI) : Multiplier(botAI, "azgalor melee dps control avoidance multiplier") {}
+    AzgalorMeleeDpsControlAvoidanceMultiplier(PlayerbotAI* botAI)
+        : Multiplier(botAI, "azgalor melee dps control avoidance multiplier") {}
     float GetValue(Action* action) override;
 };
 
@@ -118,8 +118,8 @@ public:
 class ArchimondeDisableCombatFormationMoveMultiplier : public Multiplier
 {
 public:
-    ArchimondeDisableCombatFormationMoveMultiplier(
-        PlayerbotAI* botAI) : Multiplier(botAI, "archimonde disable combat formation move multiplier") {}
+    ArchimondeDisableCombatFormationMoveMultiplier(PlayerbotAI* botAI)
+        : Multiplier(botAI, "archimonde disable combat formation move multiplier") {}
     float GetValue(Action* action) override;
 };
 
--- src/Ai/Raid/Hyjal/HyjalTriggers.h
+++ src/Ai/Raid/Hyjal/HyjalTriggers.h
@@ -14,8 +14,8 @@
 class HyjalSummitBotIsNotInCombatTrigger : public Trigger
 {
 public:
-    HyjalSummitBotIsNotInCombatTrigger(
-        PlayerbotAI* botAI) : Trigger(botAI, "hyjal summit bot is not in combat") {}
+    HyjalSummitBotIsNotInCombatTrigger(PlayerbotAI* botAI)
+        : Trigger(botAI, "hyjal summit bot is not in combat") {}
     bool IsActive() override;
 };
 
@@ -24,32 +24,32 @@ public:
 class RageWinterchillPullingBossTrigger : public Trigger
 {
 public:
-    RageWinterchillPullingBossTrigger(
-        PlayerbotAI* botAI) : Trigger(botAI, "rage winterchill pulling boss") {}
+    RageWinterchillPullingBossTrigger(PlayerbotAI* botAI)
+        : Trigger(botAI, "rage winterchill pulling boss") {}
     bool IsActive() override;
 };
 
 class RageWinterchillBossEngagedByMainTankTrigger : public Trigger
 {
 public:
-    RageWinterchillBossEngagedByMainTankTrigger(
-        PlayerbotAI* botAI) : Trigger(botAI, "rage winterchill boss engaged by main tank") {}
+    RageWinterchillBossEngagedByMainTankTrigger(PlayerbotAI* botAI)
+        : Trigger(botAI, "rage winterchill boss engaged by main tank") {}
     bool IsActive() override;
 };
 
 class RageWinterchillBossCastsDeathAndDecayOnRangedTrigger : public Trigger
 {
 public:
-    RageWinterchillBossCastsDeathAndDecayOnRangedTrigger(
-        PlayerbotAI* botAI) : Trigger(botAI, "rage winterchill boss casts death and decay on ranged") {}
+    RageWinterchillBossCastsDeathAndDecayOnRangedTrigger(PlayerbotAI* botAI)
+        : Trigger(botAI, "rage winterchill boss casts death and decay on ranged") {}
     bool IsActive() override;
 };
 
 class RageWinterchillMeleeIsStandingInDeathAndDecayTrigger : public Trigger
 {
 public:
-    RageWinterchillMeleeIsStandingInDeathAndDecayTrigger(
-        PlayerbotAI* botAI) : Trigger(botAI, "rage winterchill melee is standing in death and decay") {}
+    RageWinterchillMeleeIsStandingInDeathAndDecayTrigger(PlayerbotAI* botAI)
+        : Trigger(botAI, "rage winterchill melee is standing in death and decay") {}
     bool IsActive() override;
 };
 
@@ -58,48 +58,48 @@ public:
 class AnetheronPullingBossOrInfernalTrigger : public Trigger
 {
 public:
-    AnetheronPullingBossOrInfernalTrigger(
-        PlayerbotAI* botAI) : Trigger(botAI, "anetheron pulling boss or infernal") {}
+    AnetheronPullingBossOrInfernalTrigger(PlayerbotAI* botAI)
+        : Trigger(botAI, "anetheron pulling boss or infernal") {}
     bool IsActive() override;
 };
 
 class AnetheronBossEngagedByMainTankTrigger : public Trigger
 {
 public:
-    AnetheronBossEngagedByMainTankTrigger(
-        PlayerbotAI* botAI) : Trigger(botAI, "anetheron boss engaged by main tank") {}
+    AnetheronBossEngagedByMainTankTrigger(PlayerbotAI* botAI)
+        : Trigger(botAI, "anetheron boss engaged by main tank") {}
     bool IsActive() override;
 };
 
 class AnetheronBossCastsCarrionSwarmTrigger : public Trigger
 {
 public:
-    AnetheronBossCastsCarrionSwarmTrigger(
-        PlayerbotAI* botAI) : Trigger(botAI, "anetheron boss casts carrion swarm") {}
+    AnetheronBossCastsCarrionSwarmTrigger(PlayerbotAI* botAI)
+        : Trigger(botAI, "anetheron boss casts carrion swarm") {}
     bool IsActive() override;
 };
 
 class AnetheronBotIsTargetedByInfernalTrigger : public Trigger
 {
 public:
-    AnetheronBotIsTargetedByInfernalTrigger(
-        PlayerbotAI* botAI) : Trigger(botAI, "anetheron bot is targeted by infernal") {}
+    AnetheronBotIsTargetedByInfernalTrigger(PlayerbotAI* botAI)
+        : Trigger(botAI, "anetheron bot is targeted by infernal") {}
     bool IsActive() override;
 };
 
 class AnetheronInfernalsNeedToBeKeptAwayFromRaidTrigger : public Trigger
 {
 public:
-    AnetheronInfernalsNeedToBeKeptAwayFromRaidTrigger(
-        PlayerbotAI* botAI) : Trigger(botAI, "anetheron infernals need to be kept away from raid") {}
+    AnetheronInfernalsNeedToBeKeptAwayFromRaidTrigger(PlayerbotAI* botAI)
+        : Trigger(botAI, "anetheron infernals need to be kept away from raid") {}
     bool IsActive() override;
 };
 
 class AnetheronInfernalsContinueToSpawnTrigger : public Trigger
 {
 public:
-    AnetheronInfernalsContinueToSpawnTrigger(
-        PlayerbotAI* botAI) : Trigger(botAI, "anetheron infernals continue to spawn") {}
+    AnetheronInfernalsContinueToSpawnTrigger(PlayerbotAI* botAI)
+        : Trigger(botAI, "anetheron infernals continue to spawn") {}
     bool IsActive() override;
 };
 
@@ -108,48 +108,48 @@ public:
 class KazrogalPullingBossTrigger : public Trigger
 {
 public:
-    KazrogalPullingBossTrigger(
-        PlayerbotAI* botAI) : Trigger(botAI, "kaz'rogal pulling boss") {}
+    KazrogalPullingBossTrigger(PlayerbotAI* botAI)
+        : Trigger(botAI, "kaz'rogal pulling boss") {}
     bool IsActive() override;
 };
 
 class KazrogalBossEngagedByMainTankTrigger : public Trigger
 {
 public:
-    KazrogalBossEngagedByMainTankTrigger(
-        PlayerbotAI* botAI) : Trigger(botAI, "kaz'rogal boss engaged by main tank") {}
+    KazrogalBossEngagedByMainTankTrigger(PlayerbotAI* botAI)
+        : Trigger(botAI, "kaz'rogal boss engaged by main tank") {}
     bool IsActive() override;
 };
 
 class KazrogalBossEngagedByAssistTanksTrigger : public Trigger
 {
 public:
-    KazrogalBossEngagedByAssistTanksTrigger(
-        PlayerbotAI* botAI) : Trigger(botAI, "kaz'rogal boss engaged by assist tanks") {}
+    KazrogalBossEngagedByAssistTanksTrigger(PlayerbotAI* botAI)
+        : Trigger(botAI, "kaz'rogal boss engaged by assist tanks") {}
     bool IsActive() override;
 };
 
 class KazrogalLowManaBotsNeedEscapePathTrigger : public Trigger
 {
 public:
-    KazrogalLowManaBotsNeedEscapePathTrigger(
-        PlayerbotAI* botAI) : Trigger(botAI, "kaz'rogal low mana bots need escape path") {}
+    KazrogalLowManaBotsNeedEscapePathTrigger(PlayerbotAI* botAI)
+        : Trigger(botAI, "kaz'rogal low mana bots need escape path") {}
     bool IsActive() override;
 };
 
 class KazrogalBotIsLowOnManaTrigger : public Trigger
 {
 public:
-    KazrogalBotIsLowOnManaTrigger(
-        PlayerbotAI* botAI) : Trigger(botAI, "kaz'rogal bot is low on mana") {}
+    KazrogalBotIsLowOnManaTrigger(PlayerbotAI* botAI)
+        : Trigger(botAI, "kaz'rogal bot is low on mana") {}
     bool IsActive() override;
 };
 
 class KazrogalMarkDealsShadowDamageTrigger : public Trigger
 {
 public:
-    KazrogalMarkDealsShadowDamageTrigger(
-        PlayerbotAI* botAI) : Trigger(botAI, "kaz'rogal mark deals shadow damage") {}
+    KazrogalMarkDealsShadowDamageTrigger(PlayerbotAI* botAI)
+        : Trigger(botAI, "kaz'rogal mark deals shadow damage") {}
     bool IsActive() override;
 };
 
@@ -158,64 +158,64 @@ public:
 class AzgalorPullingBossTrigger : public Trigger
 {
 public:
-    AzgalorPullingBossTrigger(
-        PlayerbotAI* botAI) : Trigger(botAI, "azgalor pulling boss") {}
+    AzgalorPullingBossTrigger(PlayerbotAI* botAI)
+        : Trigger(botAI, "azgalor pulling boss") {}
     bool IsActive() override;
 };
 
 class AzgalorBossEngagedByMainTankTrigger : public Trigger
 {
 public:
-    AzgalorBossEngagedByMainTankTrigger(
-        PlayerbotAI* botAI) : Trigger(botAI, "azgalor boss engaged by main tank") {}
+    AzgalorBossEngagedByMainTankTrigger(PlayerbotAI* botAI)
+        : Trigger(botAI, "azgalor boss engaged by main tank") {}
     bool IsActive() override;
 };
 
 class AzgalorMainTankIsPositioningBossTrigger : public Trigger
 {
 public:
-    AzgalorMainTankIsPositioningBossTrigger(
-        PlayerbotAI* botAI) : Trigger(botAI, "azgalor main tank is positioning boss") {}
+    AzgalorMainTankIsPositioningBossTrigger(PlayerbotAI* botAI)
+        : Trigger(botAI, "azgalor main tank is positioning boss") {}
     bool IsActive() override;
 };
 
 class AzgalorBossEngagedByRangedTrigger : public Trigger
 {
 public:
-    AzgalorBossEngagedByRangedTrigger(
-        PlayerbotAI* botAI) : Trigger(botAI, "azgalor boss engaged by ranged") {}
+    AzgalorBossEngagedByRangedTrigger(PlayerbotAI* botAI)
+        : Trigger(botAI, "azgalor boss engaged by ranged") {}
     bool IsActive() override;
 };
 
 class AzgalorBossCastsRainOfFireOnMeleeTrigger : public Trigger
 {
 public:
-    AzgalorBossCastsRainOfFireOnMeleeTrigger(
-        PlayerbotAI* botAI) : Trigger(botAI, "azgalor boss casts rain of fire on melee") {}
+    AzgalorBossCastsRainOfFireOnMeleeTrigger(PlayerbotAI* botAI)
+        : Trigger(botAI, "azgalor boss casts rain of fire on melee") {}
     bool IsActive() override;
 };
 
 class AzgalorBotIsDoomedTrigger : public Trigger
 {
 public:
-    AzgalorBotIsDoomedTrigger(
-        PlayerbotAI* botAI) : Trigger(botAI, "azgalor bot is doomed") {}
+    AzgalorBotIsDoomedTrigger(PlayerbotAI* botAI)
+        : Trigger(botAI, "azgalor bot is doomed") {}
     bool IsActive() override;
 };
 
 class AzgalorDoomguardsMustBeControlledTrigger : public Trigger
 {
 public:
-    AzgalorDoomguardsMustBeControlledTrigger(
-        PlayerbotAI* botAI) : Trigger(botAI, "azgalor doomguards must be controlled") {}
+    AzgalorDoomguardsMustBeControlledTrigger(PlayerbotAI* botAI)
+        : Trigger(botAI, "azgalor doomguards must be controlled") {}
     bool IsActive() override;
 };
 
 class AzgalorDoomguardsMustDieTrigger : public Trigger
 {
 public:
-    AzgalorDoomguardsMustDieTrigger(
-        PlayerbotAI* botAI) : Trigger(botAI, "azgalor doomguards must die") {}
+    AzgalorDoomguardsMustDieTrigger(PlayerbotAI* botAI)
+        : Trigger(botAI, "azgalor doomguards must die") {}
     bool IsActive() override;
 };
 
@@ -224,48 +224,48 @@ public:
 class ArchimondePullingBossTrigger : public Trigger
 {
 public:
-    ArchimondePullingBossTrigger(
-        PlayerbotAI* botAI) : Trigger(botAI, "archimonde pulling boss") {}
+    ArchimondePullingBossTrigger(PlayerbotAI* botAI)
+        : Trigger(botAI, "archimonde pulling boss") {}
     bool IsActive() override;
 };
 
 class ArchimondeBossEngagedByMainTankTrigger : public Trigger
 {
 public:
-    ArchimondeBossEngagedByMainTankTrigger(
-        PlayerbotAI* botAI) : Trigger(botAI, "archimonde boss engaged by main tank") {}
+    ArchimondeBossEngagedByMainTankTrigger(PlayerbotAI* botAI)
+        : Trigger(botAI, "archimonde boss engaged by main tank") {}
     bool IsActive() override;
 };
 
 class ArchimondeBossCastsFearTrigger : public Trigger
 {
 public:
-    ArchimondeBossCastsFearTrigger(
-        PlayerbotAI* botAI) : Trigger(botAI, "archimonde boss casts fear") {}
+    ArchimondeBossCastsFearTrigger(PlayerbotAI* botAI)
+        : Trigger(botAI, "archimonde boss casts fear") {}
     bool IsActive() override;
 };
 
 class ArchimondeBossCastsAirBurstTrigger : public Trigger
 {
 public:
-    ArchimondeBossCastsAirBurstTrigger(
-        PlayerbotAI* botAI) : Trigger(botAI, "archimonde boss casts air burst") {}
+    ArchimondeBossCastsAirBurstTrigger(PlayerbotAI* botAI)
+        : Trigger(botAI, "archimonde boss casts air burst") {}
     bool IsActive() override;
 };
 
 class ArchimondeBossSummonedDoomfireTrigger : public Trigger
 {
 public:
-    ArchimondeBossSummonedDoomfireTrigger(
-        PlayerbotAI* botAI) : Trigger(botAI, "archimonde boss summoned doomfire") {}
+    ArchimondeBossSummonedDoomfireTrigger(PlayerbotAI* botAI)
+        : Trigger(botAI, "archimonde boss summoned doomfire") {}
     bool IsActive() override;
 };
 
 class ArchimondeBotStoodInDoomfireTrigger : public Trigger
 {
 public:
-    ArchimondeBotStoodInDoomfireTrigger(
-        PlayerbotAI* botAI) : Trigger(botAI, "archimonde bot stood in doomfire") {}
+    ArchimondeBotStoodInDoomfireTrigger(PlayerbotAI* botAI)
+        : Trigger(botAI, "archimonde bot stood in doomfire") {}
     bool IsActive() override;
 };
 
